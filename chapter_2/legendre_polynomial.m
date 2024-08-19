d2acd158b51172d2dba8141271ac7ab30b1cff37
linxj <linxj>
2024-08-19 09:28:35 +0800

    fix bug

--- chapter_2/legendre_polynomial.m
+++ chapter_2/legendre_polynomial.m
@@ -5,6 +5,7 @@ function [out] = legendre_polynomial(n, x) %codegen
     elseif (n == 1)
         out = x;
     else
-        out = ((2*n - 1)*x*legendre_polynomial(n - 1, x) - (n-1)*legendre_polynomial(n-2,x)) / n;
+        out = (2*n - 1)*x*legendre_polynomial((n - 1), x) - (n-1)*legendre_polynomial((n-2),x) / n;
+        %out = (2*n-1)*x*legendre_polynomial((n-1),x)-(n-1)*legendre_polynomial((n-2),x)/n;
     end
 end
