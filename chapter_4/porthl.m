d17ea3ddd7530ed5bb83223f211647673c22898c
peterlintang <jiexiaolinhj@163.com>
2024-08-27 21:03:42 +0800

    add comments

--- chapter_4/porthl.m
+++ chapter_4/porthl.m
@@ -1,7 +1,7 @@
 
 function [pd, pp] = porthl(p, vp, v)
 	% 点到直线的对称点以及垂足
-	% p [3 2 1] x/1=y/0=z/0
+	% 点p [3 2 1],  x/1=y/0=z/0 ([0 0 0] [1 0 0]表示直线)
 	p = reshape(p, 1, 3);
 	vp = reshape(vp, 1, 3);
 	v = reshape(v, 1, 3);
--- chapter_4/ptof.m
+++ chapter_4/ptof.m
@@ -1,5 +1,5 @@
 
-%example point [ 3 2 4], face 2x - 6y + 3z + 1 = 0;
+%example point [ 3 2 4], face 2x - 6y + 3z + 1 = 0; --> [2 -6 3 1]
 function [y] = ptof(p, f)
 	%点到平面的距离
 	d1 = dotab(f, [p 1]);
